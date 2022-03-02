bcfa443d2b877333ae56734a9e51837c4778dab6
Teo Vladusic <teo.vladusic@gmail.com>
2022-03-02 16:54:54 +0100

    inital setup

---
+++ app/src/main/java/com/prizma_distribucija/androiddevelopertask/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.prizma_distribucija.androiddevelopertask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
