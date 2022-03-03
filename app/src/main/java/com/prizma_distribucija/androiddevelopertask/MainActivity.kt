e9de150d17d6f3fde0af023c8e7a3b6d473b8023
Teo Vladusic <teo.vladusic@gmail.com>
2022-03-03 19:31:12 +0100

    hilt setup for dependency injection

--- app/src/main/java/com/prizma_distribucija/androiddevelopertask/MainActivity.kt
+++ app/src/main/java/com/prizma_distribucija/androiddevelopertask/MainActivity.kt
@@ -2,7 +2,9 @@ package com.prizma_distribucija.androiddevelopertask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
